b9539eec560ef9884931cd4728230a5858febc3a
Twilight995 <71645417+Twilight995@users.noreply.github.com>
2026-03-12 16:47:48 +0900

    3월 12일 2주차 수업

---
+++ dart/intro.dart
@@ -0,0 +1,48 @@
+void main() {
+//  var a = 'Hello, world!';
+
+//   print(a);
+
+//   a = 'Hello';
+//   print(a);
+  // final score = 50;
+
+  // if (score == 50) {
+  //   print('50점 입니다.');
+  // } else {
+  //   print('50점이 아닙니다.');
+  // }
+  
+  // score == 50 ? print('50') : print('50점 아님');
+  
+
+  // int score = 0;
+  // for(int i=0; i <= 10; i++) {
+  //   score++;
+  //   print('점수: $score');
+  // }
+
+
+  // int score = 0;
+  // switch (score) {
+  //   case 0:
+  //     break;
+  //   case 10:
+  //     default:
+  //     break;
+  // }
+
+  // return switch(score) {
+  //   >= 50 => print('50점 이상'),
+  //   _ => print('점수모름'), 
+  // };
+
+  // List<String> list = [];
+  // var llist = <dynamic>['A', 'B', 'C'];
+
+  var map = <String, dynamic>{'a': 'A', 'b': 'B',};
+  map = {'c': 'C', 'd': 0,};
+
+  
+
+}
