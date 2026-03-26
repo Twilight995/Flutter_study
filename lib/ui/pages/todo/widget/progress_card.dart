ed48329ca635d7076e5f557b45022bd54482ddb5
Twilight995 <71645417+Twilight995@users.noreply.github.com>
2026-03-26 16:43:09 +0900

    03월26일 4주차

--- lib/ui/pages/todo/widget/progress_card.dart
+++ lib/ui/pages/todo/widget/progress_card.dart
@@ -12,20 +12,54 @@ class ProgressCard extends StatelessWidget {
         borderRadius: .all(.circular(24)),
       ),
       child: Column(
+        spacing: 15,
         children: [
+          // 오늘의 진행률 텍스트
           DefaultTextStyle(
             style: TextStyle(
               color: Colors.white
             ),
             child: Row(
+              mainAxisAlignment: .spaceBetween,
               children: [
-                Text('오늘의 진행률'),
-                Text('60%')
+                Text('오늘의 진행률',
+                  style: TextStyle(
+                    fontWeight: .w700,
+                    fontSize: 16,
+                  ),
+                ),
+                Text('60%', 
+                  style: TextStyle(
+                    fontWeight: .bold,
+                    fontSize: 30,
+                  )
+                ,)
               ],
             ),
           ),
-          Text('A'),
-          Text('A'),
+          
+
+          // TODO: Progress bar
+          SizedBox(
+            height: 8,
+            child: LinearProgressIndicator(
+              value: 0.6,
+              borderRadius: .circular(10),
+              color: Colors.white,
+              backgroundColor: Colors.white.withValues(
+                alpha: 0.2,
+              ),
+            ),
+          ),
+          
+          //완료 텍스트
+          
+          Text('10개 중 6개 완료',
+            style: TextStyle(
+              fontSize: 14,
+              color: Colors.white.withValues(alpha: 0.6)
+            ),
+          ),
         ],
       ),
     );
--- lib/ui/pages/todo_page.dart
+++ lib/ui/pages/todo_page.dart
@@ -1,4 +1,6 @@
+import 'package:example/ui/pages/todo/widget/filter_row.dart';
 import 'package:example/ui/pages/todo/widget/progress_card.dart';
+import 'package:example/ui/pages/todo/widget/todo_list.dart';
 import 'package:flutter/material.dart';
 
 class TodoPage extends StatelessWidget {
@@ -11,22 +13,29 @@ class TodoPage extends StatelessWidget {
         child: Padding(
           padding: const .all(24),
           child: Column(
+            spacing: 24,
+            crossAxisAlignment: .stretch,
             children: [
               // 타이틀 상단
               TitleContent(),
-              ProgressCard()
+              ProgressCard(),
+              FilterRow(),
+              _buildTodoTitle(),
+              TodoList(),
             ],
           ),
         ),
       ),
     );
   }
+
+  Widget _buildTodoTitle() {
+    return Text('오늘 할 일', style: TextStyle(fontSize: 18, fontWeight: .bold));
+  }
 }
 
 class TitleContent extends StatelessWidget {
-  const TitleContent({
-    super.key,
-  });
+  const TitleContent({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -35,35 +44,20 @@ class TitleContent extends StatelessWidget {
       children: [
         // 타이틀
         Column(
-          
           crossAxisAlignment: .start,
           children: [
-            Text('안녕하세요',
-            style: TextStyle(
-              color: Color(0xFF71717A), 
-            ),),
-            Text(
-              '나의 할 일',
-              style: TextStyle(
-              fontWeight: .bold,
-              fontSize: 30,)
-              
-              )
+            Text('안녕하세요', style: TextStyle(color: Color(0xFF71717A))),
+            Text('나의 할 일', style: TextStyle(fontWeight: .bold, fontSize: 30)),
           ],
         ),
-        
+
         // TODO: 간격 주기
-        
-    
+
         // 알람 아이콘
         Container(
           padding: .all(10),
-          decoration: BoxDecoration(
-            color: Colors.grey[300], 
-            shape: .circle
-          ),
-          child: Icon(Icons.notification_add_outlined,
-          size: 36,), 
+          decoration: BoxDecoration(color: Colors.grey[300], shape: .circle),
+          child: Icon(Icons.notification_add_outlined, size: 36),
         ),
       ],
     );
